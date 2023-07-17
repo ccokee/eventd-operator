5c6d85f4801917d43f71c9cc064a37408468878f
Jorge Leopoldo Curbera Rodríguez <jcurbera.ext@acciona.com>
2023-07-17 14:13:05 +0200

    Setup build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-# Build the project
+# Build the project and push the docker image to the registry
+# Usage: ./build.sh <image_tag_base> <version>
+
 echo "Building the project..."
 go mod tidy && make generate && make manifests
 
-echo "Building the docker image..."
-make docker-build
-
-echo "Pushing the docker image..."
-make docker-push
+echo "Building and pushing the docker image..."
+IMAGE_TAG_BASE=$1 VERSION=$2 make docker-build docker-push
 
